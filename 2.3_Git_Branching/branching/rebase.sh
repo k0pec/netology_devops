b743f8e35c793e17d3f18675e7beba15f46068e1
kpechenkin <konstantinpe4enkin@yandex.ru>
2021-11-04 22:35:53 +0300

    git-rebase 2

--- 2.3_Git_Branching/branching/rebase.sh
+++ 2.3_Git_Branching/branching/rebase.sh
@@ -3,10 +3,10 @@
 
 count=1
 for param in "$@"; do
-<<<<<<< HEAD
     echo "\$@ Parameter #$count = $param"
 =======
-    echo "Parameter: $param"
+    echo "Next parameter: $param"
+    count=$(( $count + 1 ))
 done
 
 echo "====="
